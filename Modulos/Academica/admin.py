506448ff42bfec9a14592213f84bd0aa015d609a
jorgecba8989 <georgecba89@gmail.com>
2021-08-09 17:48:04 -0300

    creacion de auth y db

---
+++ Modulos/Academica/admin.py
@@ -0,0 +1,8 @@
+from Modulos.Academica.models import *
+from django.contrib import admin
+
+# Register your models here.
+admin.site.register(Carrera)
+admin.site.register(Estudiante)
+admin.site.register(Curso)
+admin.site.register(Matricula)
